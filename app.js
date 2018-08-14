a69f0f1cf94dd84c6549fae7faf2a4c12e035d45
wuwy <5422900397@qq.com>
2018-08-14 16:56:23 +0800

    更新了

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ App({
     globalData:{
         g_isPlayingMusic:false,
         g_currentMusicPostId:null,
-        doubanBase: "https://api.douban.com",
+      doubanBase: "https://douban.uieee.com",
     }
 })
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,12 +1,12 @@
 Page({
     onTap: function (event) {
-        wx.navigateTo({
-            url:"../posts/post"
-        });
-        
-        // wx.switchTab({
-        //     url: "../posts/post"
+        // wx.navigateTo({
+        //     url:"../posts/post"
         // });
+        
+        wx.switchTab({
+            url: "../posts/post"
+        });
         
     }
 })
